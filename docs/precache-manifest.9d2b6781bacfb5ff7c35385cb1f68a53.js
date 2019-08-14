dc837fbe84aa68cf5ef82742ebc0d3101f1cd61a
Muang <mr.khrongpop@gmail.com>
2019-08-14 14:24:03 +0700

    add docs for github page

---
+++ docs/precache-manifest.9d2b6781bacfb5ff7c35385cb1f68a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c4f724873ce1dafb4c7afa23cf2dc21",
+    "url": "/Portfolios/index.html"
+  },
+  {
+    "revision": "7fc33fad89aa9b81a3c0",
+    "url": "/Portfolios/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9b3ae1d50a4e014ef9ef",
+    "url": "/Portfolios/static/js/2.1331289a.chunk.js"
+  },
+  {
+    "revision": "7fc33fad89aa9b81a3c0",
+    "url": "/Portfolios/static/js/main.c7cd7fce.chunk.js"
+  },
+  {
+    "revision": "c56e030050f95231b177",
+    "url": "/Portfolios/static/js/runtime~main.d71a7ec1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolios/static/media/logo.5d5d9eef.svg"
+  }
+]);
