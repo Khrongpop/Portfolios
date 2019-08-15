1e2315d341d0ff5ecadbb2c44e3dac2e63d8a56b
Muang <mr.khrongpop@gmail.com>
2019-08-15 13:34:05 +0700

    project comp

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Title } from './MyComp'
 export default () => {
     return (
         <div>
-            <Title title="PROJECTS" />
+            <Title title="ABOUT" />
             <img className="img-thumbnail" src="https://graph.facebook.com/2960833650623279/picture?type=large" alt="KHRONGPOP PHONNGAM"/>
             <p>
                 Hi, I'm Khrongpop a software developer from Thailand. Nice to meet you.
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,31 @@
 import React from 'react'
 import { Title } from './MyComp'
-export default() => (
-    <div>
-        <Title title="PROJECTS"/>
-    </div>
-)
+import { Radio } from 'antd';
+import projects from '../data/projects.json'
+
+export default() => {
+    
+    const [select,setSelect] = React.useState("0")
+
+    return (
+        <div className="container">
+            <Title title="PROJECTS"/>
+            <center>
+                <Radio.Group defaultValue={select} buttonStyle="solid" onChange={e => {
+                    setSelect(e.target.value)
+                }}>
+                    <Radio.Button value="0">All</Radio.Button>
+                    <Radio.Button value="1">Web App</Radio.Button>
+                    <Radio.Button value="2">Mobile App</Radio.Button>
+                </Radio.Group>
+            </center>
+            {
+                projects.map(project => (
+                    <div>
+                        {project.name}
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
